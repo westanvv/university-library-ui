c0d52ef28fe727f3ec283b38f2bfe77913abd5fe
Vadym Nemchenko <westa.nvv@gmail.com>
2023-05-02 15:55:40 +0300

    Test usage of useCallback ,useMemo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,75 @@
-import React from 'react';
+import React, {Component, PureComponent, useState, memo, useCallback, useMemo} from 'react';
 
 import logo from 'src/assets/logo.svg';
 import styles from './App.module.scss';
 
-function App() {
+export interface AppProps {
+  name: string;
+}
+
+function App({name}: AppProps) {
+  const [count, setCount] = useState<number>(0);
+
+  const newCount = useMemo(() => {
+    return count + 100;
+  }, [count]);
+
+  const handleClick = useCallback(() => {
+    setCount(value => value + 1);
+  }, []);
+
   return (
     <div className={styles.root}>
       <header className={styles.header}>
         <img src={logo} className={styles.logo} alt="logo" />
-        <p>Hello</p>
+        <p>{name}</p>
+        <p>{count}</p>
         <a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <button onClick={handleClick}>Click</button>
       </header>
     </div>
   );
 }
 
-export default App;
+// class App2 extends Component<AppProps, {count: number}> {
+//   state = {
+//     count: 0,
+//   };
+//
+//   shouldComponentUpdate(nextProps, nextState, nextContext) {
+//     const {name} = this.props;
+//     const {count} = this.state;
+//
+//     if (name !== nextProps.name) {
+//       return true;
+//     }
+//
+//     return false;
+//   }
+//
+//   render() {
+//     const {name} = this.props;
+//     const {count} = this.state;
+//
+//     return (
+//       <button
+//         onClick={() => {
+//           this.setState(
+//             prevState => ({
+//               count: prevState.count + 1,
+//             }),
+//             () => {
+//               // gyhghj
+//             }
+//           );
+//         }}
+//       >
+//         Click
+//       </button>
+//     );
+//   }
+// }
+
+export default memo(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from 'src/components/App/App';
+import App from 'src/components/App';
 
 import 'src/styles/global.scss';
 
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="Library" />
   </React.StrictMode>
 );
